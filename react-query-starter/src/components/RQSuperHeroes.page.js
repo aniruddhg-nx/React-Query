23faa6b1db62774ec084e31dd21701d9b58c17b6
Aniruddh Gupta <aniruddh.gupta@nutanix.com>
2022-05-22 18:47:46 +0530

    handling-query-error-and-dev-tools

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,15 +1,19 @@
+
+   
 import { useQuery } from 'react-query'
 import axios from 'axios'
 
-const fetchSuperHeroes = () => {
-  return axios.get('http://localhost:4000/superheroes')
-}
-
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery('super-heroes', fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery('super-heroes', () => {
+    return axios.get('http://localhost:4000/superheroes')
+  })
 
   if (isLoading) {
     return <h2>Loading...</h2>
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>
   }
 
   return (
