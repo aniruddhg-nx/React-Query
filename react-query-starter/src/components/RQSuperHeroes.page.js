dbb61ae25924cec01b6800afaf41ce845dc5d2ad
Aniruddh Gupta <aniruddh.gupta@nutanix.com>
2022-05-24 00:22:49 +0530

    post-query

--- react-query-starter/src/components/RQSuperHeroes.page.js
+++ react-query-starter/src/components/RQSuperHeroes.page.js
@@ -1,32 +1,30 @@
 
    
-import { useQuery } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 import axios from 'axios';
+import { useState } from 'react';
 
 const fetchSuperHeroes =  () => {
   return axios.get('http://localhost:4000/superheroes')
 };
 
 export const RQSuperHeroesPage = () => {
-  // const { isLoading, data, isError, error, isFetching } = useQuery('super-heroes',
-  // fetchSuperHeroes);
-
-  const onSuccess = (data) =>{
-    console.log('Query was success with data : ', data);
-  }
-
-  const onError = (error) =>{
-    console.log('Query failed with error : ', error);
-  }
-
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery('super-heroes',
-  fetchSuperHeroes,
-  {
-   enabled:false,
-   onSuccess,
-   onError
-  });
+  fetchSuperHeroes);
 
+  const [name, setName] = useState('')
+  const [alterEgo, setAlterEgo] = useState('')
+
+  const addSuperHero = hero => {
+    return axios.post('http://localhost:4000/superheroes', hero)
+  }
+
+  const queryClient = useQueryClient();
+  const { mutate } =useMutation(addSuperHero, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('super-heroes')
+    }
+  });
 
   console.log({isLoading, isFetching});
   if (isLoading) {
@@ -37,9 +35,26 @@ export const RQSuperHeroesPage = () => {
     return <h2>{error.message}</h2>
   }
 
+  const handleAddHeroClick = () => {
+    mutate({ name, alterEgo })
+  }
+
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
+      <div>
+        <input
+          type='text'
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+        <input
+          type='text'
+          value={alterEgo}
+          onChange={e => setAlterEgo(e.target.value)}
+        />
+        <button onClick={handleAddHeroClick}>Add Hero</button>
+      </div>
       <button onClick = {refetch}>Refetch</button>
       {data?.data.map(hero => {
         return <div key={hero.name}>{hero.name}</div>
